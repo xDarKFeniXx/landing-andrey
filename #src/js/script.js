9498e2490a3066dae3f4c6e004211516441cf270
Andrew Menshikov <Men.a.a@yandex.ru>
2022-06-09 00:00:02 +0300

    lerning gulp and make live server

---
+++ #src/js/script.js
@@ -0,0 +1,5 @@
+    const os = require('os')
+    console.log ('operation system', os.platform())
+    console.log ('Archi', os.arch())
+    console.log ('cpus', os.cpus())
+    console.log ('Free memory', os.freemem())
